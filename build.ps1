1a88671d36148ccf189ec364ef2d0e37ac38809c
alhardy <alhardy@users.noreply.github.com>
2019-03-25 22:50:26 +1100

    adding reporting solution

--- build.ps1
+++ build.ps1
@@ -42,6 +42,19 @@ if ($LASTEXITCODE -ne 0)
 
 Copy-Item -path .\src\Extensions\artifacts\packages\*.nupkg -Destination .\nuget
 
+# reporting
+"######### Reporting #########"
+set-location ./src/Reporting
+& ./build.ps1 $args
+Set-Location $cd
+
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Copy-Item -path .\src\Reporting\artifacts\packages\*.nupkg -Destination .\nuget
+
 # aspnetcore
 "######### AspNetCore #########"
 set-location ./src/AspNetCore
